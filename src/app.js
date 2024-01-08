767e8737b9e58e78526347e9d290a603e94f1501
Jesus Campos <jesus.campos123@icloud.com>
2024-01-08 10:55:45 -0500

    Refactor server setup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 import express from "express";
-import { PORT } from "./config.js";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
