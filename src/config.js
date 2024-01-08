f4e67d780102d4b20e53e2f3f11f263f2770d088
Jesus Campos <jesus.campos123@icloud.com>
2024-01-08 13:28:31 -0500

    Add connection to mongodb

--- src/config.js
+++ src/config.js
@@ -5,3 +5,6 @@ import { NODE_ENVS } from "./constants.js";
 export const NODE_ENV = process.env.NODE_ENV || NODE_ENVS.DEVELOPMENT;
 
 export const PORT = process.env.PORT || 3000;
+
+export const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/db";
